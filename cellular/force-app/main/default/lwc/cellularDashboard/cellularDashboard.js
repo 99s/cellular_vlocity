3aa840ea733219588404868010c2a3baf7fe4703
99s <divi9e@gmail.com>
2022-04-16 22:58:18 +0530

    dashboard chartjs

--- cellular/force-app/main/default/lwc/cellularDashboard/cellularDashboard.js
+++ cellular/force-app/main/default/lwc/cellularDashboard/cellularDashboard.js
@@ -1,14 +1,131 @@
 import { api, LightningElement } from 'lwc';
 //import chartjs from '@salesforce/resourceUrl/chartjs_273'; 
+//import Chart from '../chart.js/dist/chart';
+
 import {
     OmniscriptBaseMixin
 } from 'vlocity_ins/omniscriptBaseMixin';
+import {
+    getDataHandler
+} from 'vlocity_ins/utility';
+
+class CellularDashboardDataModel{
+
+     constructor(cellularPhoneModelData, cellularRateplanData, cellularProviderData, cellularUserData){
+           this.cellularPhoneModel = cellularPhoneModelData;
+           this.cellularRateplan = cellularRateplanData;
+           this.cellularProvider = cellularProviderData;
+           this.cellularUser = cellularUserData;
+     }
+
+    
+}
 
 export default class CellularDashboard extends OmniscriptBaseMixin(LightningElement) {
-    @api cellularUser;
-    @api cellularPhoneModel;
-    @api cellularProvider;
-    @api cellularRateplan;
+
+     //myChart = require('chart.js');
+     @api cellularPhoneModelData;
+     @api cellularRateplanData;
+     @api cellularProviderData; 
+     @api cellularUserData;
+
+    connectedCallback(){
+        //this.callDashboardDR();
+        this.callOmniJsonData();
+        this.callApiData();
+    }
+
+
+//call dr direct, omnijson data,  @api , dr > ip > lwc
+
+  callApiData(){
+    console.log('<==========|cellularPhoneModelData|============> ')
+    console.log(JSON.stringify(this.cellularPhoneModelData));
+    console.log('<==========|cellularRateplanData|============> ')
+    console.log(JSON.stringify(this.cellularRateplanData));
+    console.log('<==========|cellularProviderData|============> ')
+    console.log(JSON.stringify(this.cellularProviderData));
+    console.log('<==========|cellularUserData|============> ')
+    console.log(JSON.stringify(this.cellularUserData));
+  }
+
+  callOmniJsonData(){
+    console.log('<==========||============> ')
+    console.log(this.omniJsonData);
+      let ojData = this.omniJsonData.cellularPhoneModel;
+    console.log('Data from Cellular_Dashboard_1 callOmniJsonData ==>', JSON.stringify(ojData));
+  }
+
+
+    callDashboardDR(){
+        
+ 
+        const datasource = JSON.stringify({
+            type: 'dataraptor',
+            value: {
+                bundleName: 'Cellular_Dashboard_1', //This will be dataraptor name
+                inputMap: {},
+                optionsMap: '{}'
+            }
+        })
+        getDataHandler(datasource).then(data => {
+            console.log('Data from Cellular_Dashboard_1 Dataraptor ==>', JSON.parse(JSON.stringify(data)));
+            
+        }).catch(err => {
+            console.error(`Error fetching data ==> ${JSON.stringify(err)}`);
+        });
+    }
+    
+
+
+    async handledashboardDataClick(event){
+      
+     try{
+        const params = {
+            input:{}, //JSON.stringify(inputData),
+            sClassName: 'vlocity_ins.IntegrationProcedureService',
+            sMethodName: 'cellular_dashboard',
+            options: '{}',
+        };
+        console.log('handledashboardDataClick params:', JSON.stringify(params));
+        let resp = await this.omniRemoteCall(params);
+        if (resp) {
+            console.log('cellular/dashboard data:' + JSON.stringify(resp));
+        } else {
+            console.log('Error while fetching..');
+        }
+        this.loading = false;
+     }catch(e){
+        console.log('Error handledashboardDataClick..', e);
+     }
+        
+ 
+      
+ 
+        // const inputData = {
+        //     "QuoteId": this.quoteId,
+        //     "LineItemId": lineItemId,
+        //     "Quantity": parseInt(addOnQuantity)
+        // };
+      
+
+    }
+
+    initializeChart(){
+        const chart = new Chart(ctx, {
+            type: 'line',
+            data: data,
+            options: {
+              onClick: (e) => {
+                const canvasPosition = getRelativePosition(e, chart);
+          
+                // Substitute the appropriate scale IDs
+                const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
+                const dataY = chart.scales.y.getValueForPixel(canvasPosition.y);
+              }
+            }
+          });
+    }
     
 
 }
