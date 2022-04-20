e17c678286cf6ea9f226678c498bd1c6067a087a
99s <divi9e@gmail.com>
2022-04-20 16:11:56 +0530

    login working

--- cellular/force-app/main/default/lwc/cellularLogin/cellularLogin.js
+++ cellular/force-app/main/default/lwc/cellularLogin/cellularLogin.js
@@ -3,15 +3,15 @@ import {
     OmniscriptBaseMixin
 } from 'vlocity_ins/omniscriptBaseMixin';
 
-import VerifyLogin from '@salesforce/apex/CellularLoginClass.VerifyLogin';
+// import VerifyLogin from '@salesforce/apex/CellularLoginClass.VerifyLogin';
 // import {
 //     getDataHandler
 // } from 'vlocity_ins/utility';
 
 class LoginData{
-    constructor(username, password){
-        this.loginUsername = username;
-        this.loginPassword = password;
+    constructor(uname, pass){
+        this.username = uname;
+        this.password = pass;
     }
 }
 
@@ -44,21 +44,71 @@ export default class CellularLogin extends OmniscriptBaseMixin(LightningElement)
 
     }
 
-    onclickLogin(){
+    async onclickLogin(){
         try{
+            this.textShow = '';
             let un = this.template.querySelector('[data-id="username"]').value;
             let pa = this.template.querySelector('[data-id="password"]').value;
     
             let loginData = new LoginData(un,pa);
             console.log('loginData-->', loginData);
             this.textShow = JSON.stringify(loginData);
-            let returnData = VerifyLogin(loginData);
-            console.log('returnData-->', returnData);
+            let returnData = await this.cleckLoginIP(JSON.stringify(loginData));
+            let returnJson = returnData.result.IPResult.CallLoginClassHttpAction.output; //JSON.stringify(returnData);
+            console.log('onclickLogin returnJson-->', returnJson);
+            if(returnJson != null){
+                 if(returnJson.result === true){
+                        this.textShow = 'login success!'
+                 }
+                 else{
+                    this.textShow = 'login failed !';
+                    if(returnJson.error === 'none'){
+                        this.textShow += ' check your credentials ';
+                    }
+                    else{
+                        this.textShow += returnJson.error;
+                    }
+                 }
+            }
+            else{
+                this.textShow = 'Didn\'t Receive any response from server!';
+            }
+            
         }catch(e){
             console.log('CellularLogin onclickLogin error : ', e);
             this.textShow = e.message;
         }
         
+    }
+
+    async cleckLoginIP(loginData){
+        let resp = null;
+        try{
+          
+            // const inputCredentials = {
+            //     "username": uname,
+            //     "password": pass
+            // }
+           const params = {
+               input: loginData,//JSON.stringify(inputCredentials),
+               sClassName: 'vlocity_ins.IntegrationProcedureService',
+               sMethodName: 'cellular_login',
+               options: '{}',
+           };
+           console.log('cleckLoginIP params:', JSON.stringify(params));
+            resp = await this.omniRemoteCall(params);
+           if (resp) {
+
+               console.log('cleckLoginIP response:' + JSON.stringify(resp));
+              
+           } else {
+               console.log('Error while fetching..');
+           }
+           
+        }catch(e){
+           console.log('Error cleckLoginIP..', e);
+        }
+        return resp;
     }
 
 
